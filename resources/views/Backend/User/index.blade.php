62bc21419d0e46ee138b0cebd38971e832c47a38
Ankittdr <ankittdr@gmail.com>
2021-09-17 16:46:16 +0545

    Admin Template

---
+++ resources/views/Backend/User/index.blade.php
@@ -0,0 +1,83 @@
+@extends('Backend.layouts.master')
+@push('backend-stylesheet')
+    <!-- BEGIN PAGE LEVEL STYLES -->
+    <link href="{{asset('backend/assets/css/scrollspyNav.css')}}" rel="stylesheet" type="text/css" />
+    <link href="{{asset('backend/plugins/file-upload/file-upload-with-preview.min.css')}}" rel="stylesheet" type="text/css" />
+    <!-- END PAGE LEVEL STYLES -->
+@endpush
+@section('content')
+<div class="page-header">
+    <div class="page-title">
+        <h3>Analytics Dashboard</h3>
+    </div>
+</div>
+<div class="row">
+    <div id="flFormsGrid" class="col-lg-12 layout-spacing">
+        <div class="statbox widget box box-shadow">
+            <div class="widget-header">
+                <div class="row">
+                    <div class="col-xl-6 col-md-6 col-sm-6 col-6">
+                        <h4>Forms Grid</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="widget-content widget-content-area">
+{{--
+                <form action="{{route('users.store')}}" method="post" enctype="multipart/form-data">
+                    @csrf
+                    <div class="form-row mb-6">
+                        <div class="form-group col-md-6">
+                            <label for="inputEmail4">Name</label>
+                            <input type="text" class="form-control" id="inputEmail4" placeholder="Name" name="name">
+                            <span style="color: red;">{{$errors->first('name')}}</span>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="inputEmail4">Phone No.</label>
+                            <input type="number" class="form-control" id="inputEmail4" placeholder="Phone number" name="phone_no">
+                        </div>
+
+                    </div>
+                    <div class="form-row mb-4">
+                        <div class="form-group col-md-6">
+                            <label for="inputEmail4">Email</label>
+                            <input type="email" class="form-control" id="inputEmail4" placeholder="Email" name="email">
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="inputPassword4">Password</label>
+                            <input type="password" class="form-control" id="inputPassword4" placeholder="Password" name="password">
+                        </div>
+                    </div>
+                    <div class="form-group mb-4">
+                        <label for="inputAddress">Address</label>
+                        <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St" name="address">
+                    </div>
+                    <div class="custom-file-container" data-upload-id="myFirstImage">
+                        <label>Upload (Single File) <a href="javascript:void(0)" class="custom-file-container__image-clear" title="Clear Image">x</a></label>
+                        <label class="custom-file-container__custom-file" >
+                            <input type="file" class="custom-file-container__custom-file__custom-file-input" accept="image/*" name="image">
+                            <input type="hidden" name="MAX_FILE_SIZE" value="10485760" />
+                            <span class="custom-file-container__custom-file__custom-file-control"></span>
+                        </label>
+                        <div class="custom-file-container__image-preview"></div>
+                    </div>
+                    <button type="submit" class="btn btn-primary mt-3">Register</button>
+                </form>
+--}}
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+@push('backend-scripts')
+    <!-- BEGIN PAGE LEVEL PLUGINS -->
+    <script src="{{asset('backend/assets/js/scrollspyNav.js')}}"></script>
+    <script src="{{asset('backend/plugins/file-upload/file-upload-with-preview.min.js')}}"></script>
+
+    <script>
+        //First upload
+        var firstUpload = new FileUploadWithPreview('myFirstImage')
+        //Second upload
+        var secondUpload = new FileUploadWithPreview('mySecondImage')
+    </script>
+    <!-- END PAGE LEVEL PLUGINS -->
+@endpush
